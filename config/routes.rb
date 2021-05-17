edfec2fe70022a82e5efb5c14d1cba43ad862207
jenifa-moritakakun <82855669+jenifa-moritakakun@users.noreply.github.com>
2021-05-18 07:21:53 +0900

    デプロイ

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "furima#index"
-
+  root to: "items#index"
+  resource :items
 end
