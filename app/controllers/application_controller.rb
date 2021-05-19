daffd3974a53a5e05c0514f1fe4078c004191662
jenifa-moritakakun <82855669+jenifa-moritakakun@users.noreply.github.com>
2021-05-19 11:26:21 +0900

    エラーメッセージの表示

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,6 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters 
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :encrypted_password, :family_name, :first_name, :family_name_full_width, :first_name_full_width, :birth_day])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :first_name, :family_name_full_width, :first_name_full_width, :birth_day])
   end
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,6 +7,16 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @user = User.new(create_params)
+    if @user.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
+  private
+  def create_params
+    params.require(:item).permit(:nickname, :email, :encrypted_password, :family_name, :first_name, :family_name_full_width, :first_name_full_width, :birth_day).merge(user_id: current_user.id)
+  end
 end
