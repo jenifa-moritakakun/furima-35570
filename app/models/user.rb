4096f96a47d6502c3d5b072f091b9d3cb0b26a08
jenifa-moritakakun <82855669+jenifa-moritakakun@users.noreply.github.com>
2021-05-20 18:14:54 +0900

    正常系のテストコードの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :password, format: { with: /[a-z\d]{6,}/ }
+  validates :password, format: { with:/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
   with_options presence: true do
     validates :birth_day
     validates :nickname
