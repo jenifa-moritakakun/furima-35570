e17c8a6c115ad0b6822960ce45adba4cf00dc014
jenifa-moritakakun <82855669+jenifa-moritakakun@users.noreply.github.com>
2021-05-20 12:33:41 +0900

    deviseを使用した修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,28 +2,4 @@ class ItemsController < ApplicationController
   def index
   end
 
-  def new
-    @user = User.new
-  end
-
-  def create
-    @user = User.new(create_params)
-    if @user.save
-      redirect_to root_path
-    else
-      render :new
-    end
-  end
-
-  def destroy
-    user = User.find(params[:id])
-    user.destroy
-  end
-
-  private
-
-  def create_params
-    params.require(:item).permit(:nickname, :email, :encrypted_password, :family_name, :first_name, :family_name_full_width,
-                                 :first_name_full_width, :birth_day).merge(user_id: current_user.id)
-  end
 end
