8d522815bdcfaa08d65f2e35dbc487f895e00644
jenifa-moritakakun <82855669+jenifa-moritakakun@users.noreply.github.com>
2021-05-24 12:15:07 +0900

    rubocop

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -46,9 +46,6 @@ class ItemsController < ApplicationController
   end
 
   def contributor_confirmation
-    unless current_user == @item.user
-      redirect_to root_path
-    end
+    redirect_to root_path unless current_user == @item.user
   end
-
 end
