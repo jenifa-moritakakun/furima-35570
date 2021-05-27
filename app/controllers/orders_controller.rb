18cc75f6167f82fa17a0a537568488d4e9d48e31
jenifa-moritakakun <82855669+jenifa-moritakakun@users.noreply.github.com>
2021-05-27 15:26:02 +0900

    商品情報表示

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,19 @@
 class OrdersController < ApplicationController
+  before_action :set_item, only: [:create, :index]
   def index
     @order_address = OrderAddress.new
   end
 
   def create
-    binding.pry
+  end
+
+  private
+  def create_params
+    params.require(:item).permit(:image, :name, :description, :details_category_id, :details_status_id, :shipping_charges_id,
+                                 :shipping_area_id, :shipping_days_id, :selling_price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
   end
 end
